4d6c43a6db5fc7e0656a364f6a4f1a97496fd238
Demarillac Izere <izered3@gmail.com>
2021-02-21 21:31:03 +0200

    added logout

--- IP/umurinzi/umurinzi/settings.py
+++ IP/umurinzi/umurinzi/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
