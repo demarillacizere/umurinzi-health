d2e0c7f9461470bec1da78114f9a351d50bc14ad
umedith <umedith03@gmail.com>
2021-02-23 10:04:02 +0200

    added some changes

--- IP/umurinzi/umurinzi/urls.py
+++ IP/umurinzi/umurinzi/urls.py
@@ -1,5 +1,4 @@
 """umurinzi URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
@@ -15,10 +14,8 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('health.urls')),
-     url(r'^accounts/', include('registration.backends.simple.urls')),
-
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
