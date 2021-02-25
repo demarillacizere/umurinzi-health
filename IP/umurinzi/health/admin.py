43c3aa4df2861e79e14fd69cb98410505101543c
joseingabire123@gmail.com <joseingabire123@gmail.com>
2021-02-25 12:18:49 +0200

    worked on footer

--- IP/umurinzi/health/admin.py
+++ IP/umurinzi/health/admin.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 from .models import *
 
-
 admin.site.register(Pharmacy)
 admin.site.register(Drug)
 admin.site.register(Location)
 admin.site.register(Profile)
-<<<<<<< HEAD
 admin.site.register(Insurance)
-=======
-admin.site.register(Insurance)
->>>>>>> d563433d45f503edee6194bf0ae8b65733f075ca
