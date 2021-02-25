193c4cf642790dd18b5673d0a9505e4bbee67286
joseingabire123@gmail.com <joseingabire123@gmail.com>
2021-02-25 10:12:34 +0200

    changes

--- IP/umurinzi/health/admin.py
+++ IP/umurinzi/health/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import *
 
+
 admin.site.register(Pharmacy)
 admin.site.register(Drug)
 admin.site.register(Location)
 admin.site.register(Profile)
+admin.site.register(Insurance)
